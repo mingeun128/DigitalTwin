50cb1541d2da718cb47f4d6e66937326ea75bb65
mingeun128 <52354632+mingeun128@users.noreply.github.com>
2021-12-30 18:17:54 +0900

    Add more power for moving car

--- My project/Assets/CarMove.cs
+++ My project/Assets/CarMove.cs
@@ -9,9 +9,10 @@ public class CarMove : MonoBehaviour
     public WheelCollider[] wheels = new WheelCollider[4];
     public Transform[] tires = new Transform[4]; 
     public float mortor = 5000000.0f;
-    public float power = 2000.0f;
+    public float power = 10000f;
     public float rot = 50;
     public float obstaclePosition = 230;
+    public bool isbrake = false;
     GameObject car = null;
     Rigidbody rb;
 
@@ -36,13 +37,21 @@ public class CarMove : MonoBehaviour
     private void Update()
     {
         UpdateMeshesPostion();
-        //rb.AddForce(transform.rotation * new Vector3(0, 0, power));
     }
-    /*
+    
     void FixedUpdate()
     {
         //float a = Input.GetAxis("Vertical");
-        //rb.AddForce(transform.rotation * new Vector3(0, 0, power));
+        if (isbrake)
+        {
+            rb.AddForce(transform.rotation * new Vector3(0, 0, -power));
+        }
+        else
+        {
+            rb.AddForce(transform.rotation * new Vector3(0, 0, power));
+        }
+        
+        /*
         if (car.transform.position.x < 600)
         {
             Avoidance();
@@ -61,9 +70,9 @@ public class CarMove : MonoBehaviour
         {
             wheels[i].steerAngle = steer;
         }
-        
+        */
     }
-*/
+
     void UpdateMeshesPostion()
     {
         for (int i = 0; i < 4; i++)
@@ -78,79 +87,113 @@ public class CarMove : MonoBehaviour
 
     IEnumerator Avoidance()
     {
+        isbrake = true;
         for (int i = 0; i < 4; i++)
         {
-            wheels[i].brakeTorque = mortor*5000000;
+            wheels[i].brakeTorque = mortor*500;
+            wheels[i].motorTorque = 0;
         }
         yield return new WaitForSeconds(2.0f);
+        isbrake = false;
         for (int i = 0; i < 4; i++)
         {
             wheels[i].brakeTorque = 0;
         }
-        wheels[1].motorTorque = mortor * 2;
-        wheels[3].motorTorque = mortor * 2;
         for (int i = 0; i < 2; i++)
         {
             wheels[i].steerAngle = -rot;
         }
-        
+        wheels[1].motorTorque = mortor;
+        wheels[3].motorTorque = mortor;
         yield return new WaitForSeconds(4.0f);
+        
         wheels[1].motorTorque = mortor;
         wheels[3].motorTorque = mortor;
         for (int i = 0; i < 2; i++)
         {
             wheels[i].steerAngle = 0;
         }
-        
+        isbrake = true;
+        for (int i = 0; i < 4; i++)
+        {
+            wheels[i].brakeTorque = mortor * 500;
+            wheels[i].motorTorque = 0;
+        }
         yield return new WaitForSeconds(1.5f);
-        wheels[0].motorTorque = mortor * 2;
-        wheels[2].motorTorque = mortor * 2;
-        wheels[1].motorTorque = -mortor;
-        wheels[3].motorTorque = -mortor;
+
+        isbrake = false;
         for (int i = 0; i < 2; i++)
         {
             wheels[i].steerAngle = rot;
         }
-        
+        wheels[0].motorTorque = mortor;
+        wheels[2].motorTorque = mortor;
+        //wheels[1].motorTorque = mortor;
+        //wheels[3].motorTorque = mortor;
+
         yield return new WaitForSeconds(8f);
-        wheels[1].motorTorque = mortor * 2;
-        wheels[3].motorTorque = mortor * 2;
+
+        isbrake = true;
+        for (int i = 0; i < 4; i++)
+        {
+            wheels[i].brakeTorque = mortor * 500;
+            wheels[i].motorTorque = 0;
+        }
+        yield return new WaitForSeconds(2.0f);
+
+        isbrake = false;
+        wheels[0].motorTorque = mortor;
+        wheels[2].motorTorque = mortor;
+        //wheels[1].motorTorque = mortor;
+        //wheels[3].motorTorque = mortor;
+
+        yield return new WaitForSeconds(8.0f);
+
+
         for (int i = 0; i < 2; i++)
         {
             wheels[i].steerAngle = -rot;
         }
-        yield return new WaitForSeconds(3.0f);
         wheels[1].motorTorque = mortor;
         wheels[3].motorTorque = mortor;
+        yield return new WaitForSeconds(3.0f);
+
         for (int i = 0; i < 2; i++)
         {
             wheels[i].steerAngle = 0;
         }
+        yield return new WaitForSeconds(7.0f);
         StopCoroutine("Avoidance");
         StartCoroutine("MoveCar");
     }
 
     IEnumerator MoveCar()
     {
-        while (car.transform.position.x > obstaclePosition + 370)
+        
+        while (car.transform.position.x > obstaclePosition + 400)
         {
+            isbrake = false;
             for (int i = 0; i < 4; i++)
             {
                 wheels[i].motorTorque = mortor;
             }
             yield return new WaitForSeconds(0.2f);
         }
-        if ((car.transform.position.x <= obstaclePosition + 370) && (car.transform.position.x > obstaclePosition + 270))
+        if ((car.transform.position.x <= obstaclePosition + 400) && (car.transform.position.x > obstaclePosition + 300))
         {
             StopCoroutine("MoveCar");
             StartCoroutine("Avoidance");
         }
         if (car.transform.position.x < 100)
         {
+            Debug.Log("BRAKE");
+            isbrake = true;
             for (int i = 0; i < 4; i++)
             {
                 wheels[i].brakeTorque = mortor * 500;
+                wheels[i].motorTorque = 0;
             }
+            yield return new WaitForSeconds(2.0f);
         }
 
     }
