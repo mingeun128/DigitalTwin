03a3456925831344844273c912aa67744e142819
mingeun128 <52354632+mingeun128@users.noreply.github.com>
2022-03-19 01:34:29 +0900

    two-way communication
    c++ -> unity : keyboard input
    unity -> c++ : yello car position

--- IPCPractice/ipc example/c++ client/svr.cpp
+++ IPCPractice/ipc example/c++ client/svr.cpp
@@ -4,26 +4,25 @@
 #include <string>
 #include <conio.h>
 #include <tchar.h>
+#include <thread>
 using namespace std;
 #define BUFSIZE 100
 char buf[100];
-
+int getData();
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "CLIENT" << endl;
 	
-	HANDLE hPipe, hPipe2;
-	HANDLE hHeap = GetProcessHeap();
+	HANDLE hPipe;
 	LPTSTR lpvMessage;
 	BOOL   fSuccess = FALSE;
-	BOOL   fSuccess2 = FALSE;
-	DWORD  cbRead, cbToWrite, cbWritten, dwMode;
-	TCHAR* chBuf = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+	DWORD  cbToWrite, cbWritten, dwMode;
 	LPTSTR lpszPipename = TEXT("\\\\.\\pipe\\DEVSPipe");
-	LPTSTR lpszPipename2 = TEXT("\\\\.\\pipe\\DEVSPipe2");
+	
 
 	if (argc > 1)
 		lpvMessage = argv[1];
+	thread t1(getData);
 	while (1)
 	{
 		hPipe = CreateFile(
@@ -37,20 +36,10 @@ int _tmain(int argc, _TCHAR* argv[])
 			NULL);          // no template file 
 
 							// Break if the pipe handle is valid. 
-		hPipe2 = CreateFile(
-			lpszPipename2,   // pipe name 
-			GENERIC_READ |  // read and write access 
-			GENERIC_WRITE,
-			0,              // no sharing 
-			NULL,           // default security attributes
-			OPEN_EXISTING,  // opens existing pipe 
-			0,              // default attributes 
-			NULL);          // no template file 
 
 		if (hPipe != INVALID_HANDLE_VALUE)
 			break;
-		if (hPipe2 != INVALID_HANDLE_VALUE)
-			break;
+
 
 		// Exit if an error other than ERROR_PIPE_BUSY occurs. 
 
@@ -67,11 +56,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			printf("Could not open pipe: 20 second wait timed out.");
 			return -1;
 		}
-		if (!WaitNamedPipe(lpszPipename2, 20000))
-		{
-			printf("Could not open pipe: 20 second wait timed out.");
-			return -1;
-		}
+		
 	}
 
 	// Send a message to the pipe server.
@@ -80,26 +65,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		cin >> str;
 		cbToWrite = (strlen(str.c_str())+1)*sizeof(TCHAR);
 		printf("Sending %d byte message: \"%s\"\n", cbToWrite,	str.c_str());
-
 		fSuccess = WriteFile(
 			hPipe,                  // pipe handle 
 			str.c_str(),             // message 
 			cbToWrite,              // message length 
 			&cbWritten,             // bytes written 
 			NULL);                  // not overlapped 
-
-		fSuccess2 = ReadFile(
-			hPipe2,    // pipe handle 
-			chBuf,    // buffer to receive reply 
-			BUFSIZE * sizeof(TCHAR),  // size of buffer 
-			&cbRead,  // number of bytes read 
-			NULL);    // not overlapped 
-
-		if (!fSuccess2 && GetLastError() != ERROR_MORE_DATA)
-			break;
-
-		_tprintf(TEXT("\"%s\"\n"), chBuf);
-		memset(chBuf, 0, BUFSIZE * sizeof(TCHAR));
 
 		if (!fSuccess)
 		{
@@ -109,7 +80,60 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 
 	CloseHandle(hPipe);
-	CloseHandle(hPipe2);
+	t1.join();
 
+	return 0;
+}
+int getData() 
+{
+	HANDLE hPipe2;
+	HANDLE hHeap = GetProcessHeap();
+	DWORD  cbRead = 0;
+	BOOL   fSuccess2 = FALSE;
+	LPTSTR chBuf = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+	LPTSTR lpszPipename2 = TEXT("\\\\.\\pipe\\DEVSPipe2");
+	while (1)
+	{
+		hPipe2 = CreateFile(
+			lpszPipename2,   // pipe name 
+			GENERIC_READ |  // read and write access 
+			GENERIC_WRITE,
+			0,              // no sharing 
+			NULL,           // default security attributes
+			OPEN_EXISTING,  // opens existing pipe 
+			0,              // default attributes 
+			NULL);          // no template file 
+		if (hPipe2 != INVALID_HANDLE_VALUE)
+			break;
+
+		if (!WaitNamedPipe(lpszPipename2, 20000))
+		{
+			printf("Could not open pipe: 20 second wait timed out.");
+			return -1;
+		}
+	}
+	while (1) 
+	{
+		memset(chBuf, 0, BUFSIZE * sizeof(TCHAR));
+		fSuccess2 = ReadFile(
+			hPipe2,    // pipe handle 
+			chBuf,    // buffer to receive reply 
+			BUFSIZE * sizeof(TCHAR),  // size of buffer 
+			&cbRead,  // number of bytes read 
+			NULL);    // not overlapped 
+
+		if (!fSuccess2 && GetLastError() != ERROR_MORE_DATA) {
+			_tprintf(TEXT("GLE=%d\n"), GetLastError());
+			break;
+		}
+		printf("Yello car Position is : %s \n", chBuf);
+		//_tprintf(TEXT("%s\n"), chBuf);
+		
+	}
+	CloseHandle(hPipe2);
+	HeapFree(hHeap, 0, chBuf);
+	FlushFileBuffers(hPipe2);
+	DisconnectNamedPipe(hPipe2);
+	CloseHandle(hPipe2);
 	return 0;
 }
--- My project/Assets/Client.cs
+++ My project/Assets/Client.cs
@@ -11,15 +11,21 @@ public class Client : MonoBehaviour
 {
     NamedPipeServerStream pipe;
     NamedPipeServerStream pipe2;
+    Thread t1;
+    Thread t2;
     string str="";
+    string car_position = "";
+    GameObject car = null;
+    private Vector3 carPosition;
 
     void Start()
     {
+        car = GameObject.Find("car 1203 yellow");
         Debug.Log("SERVER START");
         pipe = new NamedPipeServerStream("DEVSPipe", PipeDirection.InOut);
         pipe2 = new NamedPipeServerStream("DEVSPipe2", PipeDirection.InOut);
-        Thread t1 = new Thread(new ThreadStart(GetPipeData));
-        Thread t2 = new Thread(new ThreadStart(SendPipeData));
+        t1 = new Thread(new ThreadStart(GetPipeData));
+        t2 = new Thread(new ThreadStart(SendPipeData));
         t1.IsBackground = true;
         t1.Start();
         t2.IsBackground = true;
@@ -27,11 +33,21 @@ public class Client : MonoBehaviour
     }
     void Update()
     {
+        carPosition = car.transform.position;
+        car_position = carPosition.ToString();
         if (str.Length > 0)
         {
             Debug.Log(str);
             str = "";
         }
+    }
+    void OnDestroy()
+    {
+        t1.Abort();
+        t2.Abort();
+        //pipe.Close();
+        //pipe2.Close();
+        
     }
     private void GetPipeData()
     {
@@ -60,13 +76,21 @@ public class Client : MonoBehaviour
                 pipe2 = new NamedPipeServerStream("DEVSPipe2", PipeDirection.InOut);
                 pipe2.WaitForConnection();
             }
-            StreamWriter sw = new StreamWriter(pipe2);
-            sw.AutoFlush = true;
-            String strHello = "Hello";
-            //IntPtr pStr = Marshal.StringToHGlobalUni(strHello);
-            sw.WriteLine(strHello);
-            //Thread.Sleep(2000);
+            try
+            {
+                // Read user input and send that to the client process.
+                StreamWriter sw = new StreamWriter(pipe2);
+                sw.WriteLine(car_position);
+                sw.Flush();
+            }
+            // Catch the IOException that is raised if the pipe is broken
+            // or disconnected.
+            catch (IOException e)
+            {
+                Debug.Log(e.Message);
+            }
+            Thread.Sleep(1000);
         }
-
+        
     }
 }
